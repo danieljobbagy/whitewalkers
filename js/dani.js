3a152197f6a30f0c680b9a68e2137390f39b85e6
Dani J <danijobbagy@gmail.com>
2018-12-01 13:03:29 +0100

    delete button working properly

--- js/dani.js
+++ js/dani.js
@@ -1,10 +1,16 @@
-function deleteRow(r) {
-  var i = r.parentNode.parentNode.parentNode.rowIndex;
-  document.getElementById('characters').deleteRow(i);
+function deleteRow(button) {
+  /* var i = r.parentNode.parentNode.parentNode.rowIndex;
+  document.getElementById('characters').deleteRow(i);*/
+  var row = button.parentNode.parentNode;
+  var table = document.getElementById('table');
+  table.deleteRow(row.rowIndex);
 }
 
-function editBio() {
 
+function editBio(button) {
+  var cell = button.parentNode.firstChild;
+  var bio = document.getElementById('bioID');
+  bio.setAttribute('contentEditable', 'true');
 }
 
 function charactertable() {
@@ -12,6 +18,7 @@ function charactertable() {
   var tableRow = '';
   for (var i = 0; i < gameOfThronesCharacters.length; i += 1) {
     var house;
+    var bioID = `bio${i}`;
     if (gameOfThronesCharacters[i].house === undefined) {
       house = '';
     } else {
@@ -25,11 +32,11 @@ function charactertable() {
                     <td>
                       <img src="${gameOfThronesCharacters[i].portrait}" alt="${gameOfThronesCharacters[i].name}">
                       </td>
-                      <td> ${house} <br><img src ="/img/houses/${house}.png" alt = "${house}" >
+                      <td> ${house} <br><img src ="/img/houses/${house}.png" alt="${house}" >
                       </td>
-                      <td id="bio${i}"> ${gameOfThronesCharacters[i].bio}
+                      <td id="${bioID}" contenteditable="false"> ${gameOfThronesCharacters[i].bio}
                       </td>
-                      <td> <button onclick="edit()" id=>Edit</button><button onclick="deleteRow(this)">Delete</button>
+                      <td> <button onclick="editBio(this)">Edit</button><input type="button" value="Delete" onclick="deleteRow(this)">
                       </td>
                 </tr>
                     `;
