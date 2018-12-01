d140bd5f9ae176c7420fd7789f607b691b15484f
GMiki <gmiki91@gmail.com>
2018-12-01 22:31:34 +0100

    deadalive js update

--- js/deadalive.js
+++ js/deadalive.js
@@ -2,6 +2,7 @@ function deadalive() {
   var deadAliveTable = document.querySelector('#deadoralive');
   var tableRow = '';
   for (var i = 0; i < gameOfThronesCharacters.length; i++) {
+    alert('for');
     tableRow += `
     <tr>
        <td>${gameOfThronesCharacters[i].name}</td>
@@ -9,6 +10,7 @@ function deadalive() {
     </tr>
   `;
   }
+  alert('after');
   deadAliveTable.innerHTML = tableRow;
 }
 deadalive();
