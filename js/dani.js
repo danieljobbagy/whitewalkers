adcab903ecffeb531b54b004c927d6c76eed7926
Dani J <danijobbagy@gmail.com>
2018-12-01 08:32:25 +0100

    house name to begin with capital letter

--- js/dani.js
+++ js/dani.js
@@ -3,35 +3,33 @@ function deleteRow(r) {
   document.getElementById('characters').deleteRow(i);
 }
 
+function editBio() {
+
+}
 
 function charactertable() {
   var charactersTableElement = document.querySelector('#characters');
-  var tableRow;
+  var tableRow = '';
   for (var i = 0; i < gameOfThronesCharacters.length; i += 1) {
-    var firstname;
     var house;
     if (gameOfThronesCharacters[i].house === undefined) {
       house = '';
     } else {
-      house = gameOfThronesCharacters[i].house;
-    }
-    if (gameOfThronesCharacters[i].name.split(' ').length > 1) {
-      firstname = gameOfThronesCharacters[i].name.split(' ').slice(0, -1);
-    } else {
-      firstname = gameOfThronesCharacters[i].name;
+      var prehouse = gameOfThronesCharacters[i].house;
+      house = prehouse.charAt(0).toUpperCase() + prehouse.slice(1);
     }
     tableRow +=
       `
                 <tr>
                     <td>${gameOfThronesCharacters[i].name}</td>
                     <td>
-                      <img src="/img/${firstname}.png" alt="${gameOfThronesCharacters[i].name}">
+                      <img src="${gameOfThronesCharacters[i].portrait}" alt="${gameOfThronesCharacters[i].name}">
                       </td>
                       <td> ${house} <br><img src ="/img/houses/${house}.png" alt = "${house}" >
                       </td>
-                      <td> ${gameOfThronesCharacters[i].bio}
+                      <td id="bio${i}"> ${gameOfThronesCharacters[i].bio}
                       </td>
-                      <td> <button onclick="edit()">Edit</button><button onclick="deleteRow(this)">Delete</button>
+                      <td> <button onclick="edit()" id=>Edit</button><button onclick="deleteRow(this)">Delete</button>
                       </td>
                 </tr>
                     `;
