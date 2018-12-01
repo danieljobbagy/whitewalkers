f5873ec2cb33190c005b7169ee511b60eec50761
GMiki <gmiki91@gmail.com>
2018-12-01 23:56:48 +0100

    abc and column sorted

--- js/deadalive.js
+++ js/deadalive.js
@@ -1,16 +1,46 @@
 function deadalive() {
   var deadAliveTable = document.querySelector('#deadoralive');
   var tableRow = '';
-  for (var i = 0; i < gameOfThronesCharacters.length; i++) {
-    alert('for');
-    tableRow += `
-    <tr>
-       <td>${gameOfThronesCharacters[i].name}</td>
-       <td>${gameOfThronesCharacters[i].dead}</td>
-    </tr>
-  `;
+  var temp;
+  var dead = 0;
+  var alive = 0;
+  for (var i = gameOfThronesCharacters.length-1; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (gameOfThronesCharacters[j].name > gameOfThronesCharacters[j + 1].name) {
+        temp = gameOfThronesCharacters[j];
+        gameOfThronesCharacters[j] = gameOfThronesCharacters[j + 1];
+        gameOfThronesCharacters[j + 1] = temp;
+      }
+    }
   }
-  alert('after');
+  for ( i = 0; i < gameOfThronesCharacters.length; i++) {
+    if (gameOfThronesCharacters[i].dead === true) {
+      dead++;
+      tableRow += `
+      <tr>
+        <td></td>
+        <td>${gameOfThronesCharacters[i].name}</td>
+      </tr>
+      `;
+    } else {
+      alive++;
+      tableRow += `
+      <tr>
+        <td>${gameOfThronesCharacters[i].name}</td>
+        <td></td>
+      </tr>
+      `;
+    }
+  }
+  tableRow += `
+  <tr></tr>
+  <tr>
+    <td>Össz:</td>
+  </tr>
+  <tr>
+    <td>${alive}</td>
+    <td>${dead}</td>
+  </tr> `;
   deadAliveTable.innerHTML = tableRow;
 }
 deadalive();
