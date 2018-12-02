a50aa64698481039c2e02ea4bd0c45f20271cb06
GMiki <gmiki91@gmail.com>
2018-12-02 10:57:12 +0100

    without css aligning

---
+++ js/organisations.js
@@ -0,0 +1,32 @@
+/* globals gameOfThronesCharacters */
+
+function organisations() {
+  var organisationsTableElement = document.querySelector('#organisations');
+  var tableRow = '';
+  var nightwatch = 0;
+  var royalguard = 0;
+  for (var i = 0; i < gameOfThronesCharacters.length; i += 1) {
+    if (gameOfThronesCharacters[i].organization === 'nightwatch') {
+      nightwatch += 1;
+    }
+    tableRow =
+      `
+                <tr>
+                    <td>Nightwatch</td>
+                    <td>${nightwatch}
+                </tr>
+                    `;
+    if (gameOfThronesCharacters[i].organization === 'royalguard') {
+      royalguard += 1;
+    }
+    tableRow +=
+      `
+                <tr>
+                    <td>Royalguard</td>
+                    <td>${royalguard}
+                </tr>
+                    `;
+  }
+  organisationsTableElement.innerHTML = tableRow;
+}
+organisations();
