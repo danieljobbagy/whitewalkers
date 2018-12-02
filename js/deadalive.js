3b2bbd63ff376f32c2d4c2d111e0f188503adc0d
GMiki <gmiki91@gmail.com>
2018-12-02 16:21:56 +0100

    updated dropdown

---
+++ js/deadalive.js
@@ -0,0 +1,46 @@
+function deadalive() {
+  var deadAliveTable = document.querySelector('#deadoralive');
+  var tableRow = '';
+  var temp;
+  var dead = 0;
+  var alive = 0;
+  for (var i = gameOfThronesCharacters.length-1; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (gameOfThronesCharacters[j].name > gameOfThronesCharacters[j + 1].name) {
+        temp = gameOfThronesCharacters[j];
+        gameOfThronesCharacters[j] = gameOfThronesCharacters[j + 1];
+        gameOfThronesCharacters[j + 1] = temp;
+      }
+    }
+  }
+  for ( i = 0; i < gameOfThronesCharacters.length; i++) {
+    if (gameOfThronesCharacters[i].dead === true) {
+      dead++;
+      tableRow += `
+      <tr>
+        <td></td>
+        <td>${gameOfThronesCharacters[i].name}</td>
+      </tr>
+      `;
+    } else {
+      alive++;
+      tableRow += `
+      <tr>
+        <td>${gameOfThronesCharacters[i].name}</td>
+        <td></td>
+      </tr>
+      `;
+    }
+  }
+  tableRow += `
+  <tr></tr>
+  <tr>
+    <td>Össz:</td>
+  </tr>
+  <tr>
+    <td>${alive}</td>
+    <td>${dead}</td>
+  </tr> `;
+  deadAliveTable.innerHTML = tableRow;
+}
+deadalive();
